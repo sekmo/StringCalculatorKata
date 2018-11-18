1bb76e87b57f40d4b18ac4ceb5ad073d574df5cd
Francesco Mari <francescojjmari@gmail.com>
2018-11-19 00:37:24 +0100

    Make add work with two numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,8 @@ class StringCalculator
 
     numbers = numbers_from(input)
     return numbers.first if numbers.size == 1
+
+    return numbers[0] + numbers[1] if numbers.size == 2
   end
 
   def self.numbers_from(string)
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,9 @@ describe StringCalculator do
     it 'returns the number if the input is just one number' do
       expect(StringCalculator.add("42")).to eq(42)
     end
+
+    it 'returns the sum of the numbers if the input is two numbers' do
+      expect(StringCalculator.add("42,5")).to eq(47)
+    end
   end
 end
