b3393823820ed4aa0d9f1af48a51abfb226704a0
Francesco Mari <francescojjmari@gmail.com>
2018-11-19 00:34:38 +0100

    Make add method work with 1 number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,12 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
+
+    numbers = numbers_from(input)
+    return numbers.first if numbers.size == 1
+  end
+
+  def self.numbers_from(string)
+    string.split(",").map(&:to_i)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ describe StringCalculator do
     it 'returns 0 with an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the number if the input is just one number' do
+      expect(StringCalculator.add("42")).to eq(42)
+    end
   end
 end
