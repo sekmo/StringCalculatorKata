22b09064021e0174cb4c0ddb12bf1a18651cdf86
Francesco Mari <francescojjmari@gmail.com>
2018-12-07 00:25:51 +0100

    Refactoring for regex

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
   STANDARD_DELIMITERS = ["\n", ","]
+  NUMBERS_AND_DELIMITERS_REGEX = /(?:\/\/(.)\n)?(.*)/m
 
   def self.add(input)
     numbers = numbers_from(input)
@@ -7,12 +8,12 @@ class StringCalculator
   end
 
   def self.numbers_from(string)
-    numbers_subtring = string.match(/\/\/.\n(.*)/).to_a[1] || string
+    numbers_subtring = string.match(NUMBERS_AND_DELIMITERS_REGEX).to_a[2]
     numbers_subtring.split(/[#{delimiters_from(string).join}]/).map(&:to_i)
   end
 
   def self.delimiters_from(string)
-    custom_delimiter = string.match(/\/\/(.)/).to_a[1]
+    custom_delimiter = string.match(NUMBERS_AND_DELIMITERS_REGEX).to_a[1]
     return STANDARD_DELIMITERS + [custom_delimiter].compact
   end
 end
